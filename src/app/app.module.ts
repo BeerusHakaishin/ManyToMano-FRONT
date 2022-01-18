c83e41f77e8edde3493de80086fe8252f3e69c9e
Ismaël Meziani <meziani-i@outlook.fr>
2022-01-18 23:19:23 +0100

    setting up bootstrap and angular material dependancies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
+// Module's import below
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// Here For Illustrate how works Angular Material , this will be in external module later
+import { MatSliderModule } from '@angular/material/slider';
+
+// Component's import below
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +16,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
