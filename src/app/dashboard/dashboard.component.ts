89f78b9e32036bf341dcc314c7a8a7562386abfe
Ismaël Meziani <meziani-i@outlook.fr>
2022-01-20 09:51:07 +0100

    progressbar

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {COMMA, ENTER} from '@angular/cdk/keycodes';
-import {MatChipInputEvent} from '@angular/material/chips';
-
-
-import { Fruit } from './fruit.model';
 
 @Component({
   selector: 'app-dashboard',
@@ -13,29 +8,6 @@ import { Fruit } from './fruit.model';
 export class DashboardComponent implements OnInit {
   selected = '';
 
-  addOnBlur = true;
-  readonly separatorKeysCodes = [ENTER, COMMA] as const;
-  fruits: Fruit[] = [{name: 'Frais de port gratuit'}, {name: 'Couleur'}];
-
-  add(event: MatChipInputEvent): void {
-    const value = (event.value || '').trim();
-
-    // Add our fruit
-    if (value) {
-      this.fruits.push({name: value});
-    }
-
-    // Clear the input value
-    event.chipInput!.clear();
-  }
-
-  remove(fruit: Fruit): void {
-    const index = this.fruits.indexOf(fruit);
-
-    if (index >= 0) {
-      this.fruits.splice(index, 1);
-    }
-  }
   ngOnInit(): void {
   }
 
--- src/app/dashboard/fruit.model.ts
+++
@@ -1,3 +0,0 @@
-export class Fruit {
-    name!: string;
-}
