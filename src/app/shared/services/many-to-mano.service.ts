4f7f56df4357087e1708236e4bf0004b0725677e
Ismaël Meziani <meziani-i@outlook.fr>
2022-01-20 17:07:07 +0100

    add some model and service methods

--- src/app/shared/services/many-to-mano.service.ts
+++ src/app/shared/services/many-to-mano.service.ts
@@ -6,6 +6,9 @@ import { url } from 'src/environments/environment';
 
 // Import necessary models below 
 import { Category } from '../models/Category.model';
+import { Room } from '../models/Room.model';
+import { Article } from '../models/Article.model';
+import { Cart } from '../models/Cart.model';
 
 @Injectable({
   providedIn: 'root'
@@ -14,8 +17,10 @@ export class ManyToManoService {
 
   constructor(private http: HttpClient) { }
 
-
   // Pour récuperer toutes les rooms 
+  public getAllRooms(): Observable<Room[]> {
+    return this.http.get<Room[]>(url + 'rooms');
+  }
 
   // Pour récuperer toutes les catégories 
   public getAllCategories(): Observable<Category[]> {
@@ -23,10 +28,23 @@ export class ManyToManoService {
   }
 
   // Pour récuperer toutes les articles
+  public getAllArticles(): Observable<Article[]> {
+    return this.http.get<Article[]>(url + 'articles');
+  }
 
   // Pour récuperer toutes les inspirations
 
+
   // Pour récuperer tout les seller 
+
+
+  // Pour récuperer toute les articles commandés (cart-line)
+
+
+  // Pour récuperer le panier 
+  public getCart(): Observable<Cart[]> {
+    return this.http.get<Cart[]>(url + 'cart');
+  }
 
   // Pour récuperer tout les articles d'une category id
 
